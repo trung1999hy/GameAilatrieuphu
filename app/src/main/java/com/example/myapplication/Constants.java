97f02c4e76c1121e940fbf37039435846bb82e4a
Trần Nam <65379025+tranam1312@users.noreply.github.com>
2023-06-07 09:17:03 +0700

    change key

--- app/src/main/java/com/example/myapplication/Constants.java
+++ app/src/main/java/com/example/myapplication/Constants.java
@@ -3,7 +3,7 @@ package com.example.myapplication;
 public class Constants {
     public static final String KEY_COIN = "key_7"; // 5 coin
 
-public static final String KEY_10_COIN = "key_8"; // 100 coin
+    public static final String KEY_10_COIN = "key_8"; // 100 coin
     public static final String KEY_20_COIN = "key_9"; // 150 coin
     public static final String KEY_50_COIN = "key_10"; // 300 coin
     public static final String KEY_100_COIN = "key_11"; // 500 coin
